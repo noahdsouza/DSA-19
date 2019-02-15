64b085601a6635e82ff069d8c74b08c8a7fa41dd
Noah D'Souza <noah.dsouza@students.olin.edu>
2019-02-14 21:28:57 -0500

    brb jumping off of west hall into Nick Steelmans loving arms

--- sorting/day00/src/InsertionSort.java
+++ sorting/day00/src/InsertionSort.java
@@ -4,15 +4,26 @@ public class InsertionSort extends SortAlgorithm {
      * Use the insertion sort algorithm to sort the array
      *
      * TODO
-     * Best-case runtime:
-     * Worst-case runtime:
-     * Average-case runtime:
+     * Best-case runtime: O(n) when the list is already sorted
+     * Worst-case runtime: O(n^2) when the list is sorted in reverse
+     * Average-case runtime: O(n^2) again, just less time if it's not the worst case
      *
-     * Space-complexity:
+     * Space-complexity: O(1) since no new arrays are made
      */
     @Override
     public int[] sort(int[] array) {
         // TODO
-        return new int[0];
+        int i = 1;
+        while (i < array.length) {
+            int x = array[i];
+            int j = i-1;
+            while (j>=0 && array[j]>x) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = x;
+            i++;
+        }
+        return array;
     }
 }
--- sorting/day00/src/MergeSort.java
+++ sorting/day00/src/MergeSort.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MergeSort extends SortAlgorithm {
 
@@ -18,7 +21,23 @@ public class MergeSort extends SortAlgorithm {
     @Override
     public int[] sort(int[] array) {
         // TODO
-        return new int[0];
+        if (array.length <= INSERTION_THRESHOLD) {
+            InsertionSort insort = new InsertionSort();
+            return insort.sort(array);
+        } else {
+            int mp = array.length/2;
+
+            int[] left = new int[mp];
+            System.arraycopy(array,0,left,0,mp);
+
+            int[] right = new int[array.length-mp];
+            System.arraycopy(array,mp,right,0,array.length-mp);
+
+            int[] outleft = sort(left);
+            int[] outright = sort(right);
+//            System.out.println(Arrays.toString(merge(outleft,outright)));
+            return merge(outleft,outright);
+        }
     }
 
     /**
@@ -27,7 +46,33 @@ public class MergeSort extends SortAlgorithm {
      */
     public int[] merge(int[] a, int[] b) {
         // TODO
-        return new int[0];
+        int[] ab = new int[a.length+b.length];
+        int aind = 0;
+        int bind = 0;
+        int abind = 0;
+        while (abind<a.length+b.length) {
+            if (bind == b.length) {
+                ab[abind] = a[aind];
+                aind++;
+                abind++;
+            } else if (aind == a.length) {
+                ab[abind] = b[bind];
+                bind++;
+                abind++;
+            } else if (a[aind] <= b[bind]) {
+                ab[abind] = a[aind];
+                aind++;
+                abind++;
+//                System.out.println(Arrays.toString(ab));
+            } else {
+                ab[abind] = b[bind];
+                bind++;
+                abind++;
+//                System.out.println(Arrays.toString(ab));
+            }
+        }
+//        System.out.println(Arrays.toString(ab));
+        return ab;
     }
 
 }
