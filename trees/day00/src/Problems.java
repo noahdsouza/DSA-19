995e65cc1a31eb9cf9f5c72c6082bb4f66d3ed80
Noah D'Souza <noah.dsouza@students.olin.edu>
2019-03-06 13:29:05 -0500

    BST and minimal height finished

--- trees/day00/src/Problems.java
+++ trees/day00/src/Problems.java
@@ -6,13 +6,26 @@ public class Problems {
         // TODO
         Collections.sort(values);
         BinarySearchTree<Integer> oof = new BinarySearchTree<>();
-        
-
-        return new BinarySearchTree<>();
+        hamburgerHelper(0,values.size(),values,oof);
+        return oof;
     }
-    public void helper() {
-        // uhhhhhhh
+    public static BinarySearchTree<Integer> hamburgerHelper(int start,int end, List<Integer> vals, BinarySearchTree<Integer> oof) {
+//        its like my BST helper function but recursively uses
+//        the median of a sublist as a parent
+        if (start == end) {
+            return oof;
+        }
+        List<Integer> temp = vals.subList(start,end);
+        int median = (temp.size())/2;
+        if (median == 0) {
+            oof.add(temp.get(0));
+            return oof;
+        }
+        oof.add(temp.get(median));
 
+        hamburgerHelper(0,median,temp,oof);
+        hamburgerHelper(median,temp.size(),temp,oof);
+        return oof;
     }
 
     public static boolean isIsomorphic(TreeNode n1, TreeNode n2) {
