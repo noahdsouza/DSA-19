6cd6b48573a0b883e20e4dba0f7a303d6eea2ce2
Noah D'Souza <noah.dsouza@students.olin.edu>
2019-04-26 14:56:01 -0400

    Adding day00

--- DP/dp00/src/DiceRollSum.java
+++ DP/dp00/src/DiceRollSum.java
@@ -1,10 +1,45 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
     // Runtime: TODO
+        // Pretty sure this is O(n*c)
     // Space: TODO
+        // O(n)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+        return helper(N,new HashMap<Integer, Integer>());
+    }
+
+    private static int helper(int N, HashMap<Integer, Integer> subs) {
+//        subs is a Hashmap with the current, iterated value of N as the key
+//        the number of solution found for that N is the value
+
+        /** Start subproblems */
+//        base case
+        if(subs.containsKey(N)) {
+            return subs.get(N);
+        }
+//        easy shit where you can do 0 to N dice
+        if(N <= 6) {
+            int permutation = (int) Math.pow(2, N-1);
+            if(N<=0) {
+                subs.put(N, 1);
+            } else {
+                subs.put(N, permutation);
+            }
+        }
+//        less easy shit where N is a thicc boi
+        else {
+            int counter = 0;
+            for(int i=1; i<=6; i++) {
+//                woot woot recursion ooh baby
+//                this'll give every solution underneath N until we hit the earlier cases
+                /** Memoize, bitch */
+                counter += helper(N-i, subs);
+            }
+            subs.put(N, counter);
+        }
+        return subs.get(N);
     }
 
 }
--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,39 @@
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence {
 
+    private static int counter = 0;
+
     // Runtime: TODO
+//    O(n^2)
     // Space: TODO
+//    O(1)
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+        int n = A.length;
+        counter = 0;
+        helper(A,n);
+        return counter;
+    }
+    private static int helper(int[] A, int n) {
+//        get dem base cases tho
+        if(n==0) {
+            return 0;
+        }
+        if(n==1) {
+            return 1;
+        }
+        int res;
+        int maxEnd = 1;
+        for(int i=1; i<n; i++) {
+            res = helper(A, i);
+            if(A[i-1]<A[n-1] && res+1>maxEnd) {
+                maxEnd = res+1;
+            }
+        }
+        if(counter<maxEnd) {
+            counter = maxEnd;
+        }
+        return maxEnd;
     }
 }
